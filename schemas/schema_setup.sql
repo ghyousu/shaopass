420e2905870b59dc9d2fcb4abe2291a9fb20a8f2
Mason You <masonyou.su@gmail.com>
2021-12-29 18:38:31 -0500

    updated schema to use enums, and added user table

--- schemas/schema_setup.sql
+++ schemas/schema_setup.sql
@@ -1,24 +1,50 @@
-DROP SCHEMA IF EXISTS ohs_shao CASCADE;
-DROP TABLE  IF EXISTS ohs_shao.student CASCADE;
+DROP TABLE  IF EXISTS ohs_shao.users CASCADE;
+DROP TYPE   IF EXISTS youUserRole CASCADE;
+DROP TABLE  IF EXISTS ohs_shao.classes CASCADE;
 DROP TABLE  IF EXISTS ohs_shao.breaks CASCADE;
+DROP TYPE   IF EXISTS youBreakType CASCADE;
+DROP TYPE   IF EXISTS youPassType CASCADE;
+DROP TABLE  IF EXISTS ohs_shao.student CASCADE;
+DROP TYPE   IF EXISTS youClassName CASCADE;
+DROP SCHEMA IF EXISTS ohs_shao CASCADE;
+
+CREATE TYPE youUserRole  AS ENUM ('teacher', 'student');
+CREATE TYPE youClassName AS ENUM ('901', '902', '903', '904');
+CREATE TYPE youBreakType AS ENUM ('Bathroom', 'Water', 'Nurse', 'Other');
+CREATE TYPE youPassType  AS ENUM ('A', 'B', 'C', 'S');
 
 CREATE SCHEMA IF NOT EXISTS ohs_shao;
 
+CREATE TABLE IF NOT EXISTS ohs_shao.users(
+   user_name VARCHAR(100) NOT NULL,
+   pw VARCHAR(255) NOT NULL,
+   role youUserRole NOT NULL,
+   PRIMARY KEY(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS ohs_shao.classes (
+   class_id youClassName NOT NULL,
+   dow INT NOT NULL,
+   time_start TIMESTAMPTZ NOT NULL,
+   time_stop  TIMESTAMPTZ NOT NULL,
+   PRIMARY KEY (class_id, dow)
+);
+
 CREATE TABLE IF NOT EXISTS ohs_shao.student (
    student_id INT GENERATED ALWAYS AS IDENTITY,
-   fname VARCHAR(255) NOT NULL,
-   lname VARCHAR(255) NOT NULL,
-   class VARCHAR(255) NOT NULL,
+   fname VARCHAR(50) NOT NULL,
+   lname VARCHAR(50) NOT NULL,
+   class youClassName NOT NULL,
    PRIMARY KEY(student_id)
 );
 
 CREATE TABLE IF NOT EXISTS ohs_shao.breaks (
    break_id INT GENERATED ALWAYS AS IDENTITY,
    student_id INT,
-   break_type VARCHAR(255),
-   pass_type VARCHAR(255),
+   break_type youBreakType,
+   pass_type youPassType,
    time_out TIMESTAMPTZ NOT NULL DEFAULT NOW(),
    time_in  TIMESTAMPTZ DEFAULT NOW(),
    PRIMARY KEY(break_id),
-   FOREIGN KEY(student_id) REFERENCES ohs_shao.student(student_id) ON DELETE CASCADE
+   FOREIGN KEY(student_id) REFERENCES ohs_shao.student(student_id)
 );
