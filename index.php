46e49dc53c5f23c78b48e5975a91bc18b4c652be
Mason You <masonyou.su@gmail.com>
2021-12-29 23:44:52 -0500

    moved javascript to be after php script so that header works well

--- index.php
+++ index.php
@@ -2,7 +2,49 @@
   <head>
      <title>Student Break Tracker</title>
 
-     <?php require_once("common.php"); ?>
+     <?php
+         require_once("common.php");
+
+         setlocale(LC_ALL,'C.UTF-8');
+
+         session_start();
+
+         if (!isset($_SESSION['LOGGED_IN']))
+         {
+            header("location: /login.php");
+         }
+
+         if ($_SERVER['REQUEST_METHOD'] === 'POST')
+         {
+            // var_dump($_POST);
+            // die("<br/>temp");
+            // sample output:
+            //    array(4) {
+            //       ["student_id"]=> string(9) "35"
+            //       ["break_type"]=> string(8) "Bathroom"
+            //       ["pass_type"]=> string(1) "E"
+            //       ["submit"]=> string(6) "Check In"
+            //    }
+
+               $student_id  = $_POST['student_id'];
+               $break_type  = $_POST['break_type'];
+               $pass_type   = $_POST['pass_type'];
+               $is_checkout = ($_POST['submit'] == "Check Out");
+
+               $break_id_session_key = getBreakIdSessionKey($student_id);
+
+               if ($is_checkout)
+               {
+                  $break_id = checkoutStudent($student_id, $break_type, $pass_type);
+
+                  $_SESSION[$break_id_session_key] = $break_id;
+               }
+               else
+               {
+                  checkinStudent($student_id, $_SESSION[$break_id_session_key]);
+               }
+            }
+     ?>
 
      <script type="text/javascript">
       function updateStudentNameColor()
@@ -191,47 +233,9 @@
 
      </script>
 
-     <?php
-         setlocale(LC_ALL,'C.UTF-8');
 
-         session_start();
 
-         if (!isset($_SESSION['LOGGED_IN']))
-         {
-            header("location: /login.php");
-         }
 
-         if ($_SERVER['REQUEST_METHOD'] === 'POST')
-         {
-            // var_dump($_POST);
-            // die("<br/>temp");
-            // sample output:
-            //    array(4) {
-            //       ["student_id"]=> string(9) "35"
-            //       ["break_type"]=> string(8) "Bathroom"
-            //       ["pass_type"]=> string(1) "E"
-            //       ["submit"]=> string(6) "Check In"
-            //    }
-
-               $student_id  = $_POST['student_id'];
-               $break_type  = $_POST['break_type'];
-               $pass_type   = $_POST['pass_type'];
-               $is_checkout = ($_POST['submit'] == "Check Out");
-
-               $break_id_session_key = getBreakIdSessionKey($student_id);
-
-               if ($is_checkout)
-               {
-                  $break_id = checkoutStudent($student_id, $break_type, $pass_type);
-
-                  $_SESSION[$break_id_session_key] = $break_id;
-               }
-               else
-               {
-                  checkinStudent($student_id, $_SESSION[$break_id_session_key]);
-               }
-            }
-     ?>
   </head>
 
   <body onload="on_page_loaded()">
