249599b0ad895c3dff8616609e88cd5bfdd0fbc1
Mason You <masonyou.su@gmail.com>
2022-01-08 14:26:31 -0500

    restored teacher's note text box

--- index.php
+++ index.php
@@ -265,7 +265,7 @@
      </tr>
 
      <tr>
-     <td>
+     <td rowspan="2">
         <form action='/index.php' method='POST' enctype='multipart/form-data'>
            <?php
               echo '<h2 style="margin-block-end: -0.5em">Select your name:</h2><br/>';
@@ -297,15 +297,17 @@
      </td>
      </tr>
 
-     <!--
      <tr>
-     <td style="vertical-align: baseline">
-        <form action='/submit_notes.php' method='POST' enctype='multipart/form-data'>
-          <textarea id="notes_textarea" name="notes" placeholder="Enter teacher's comment here ..." style="font-size: 1.5em; width: 300px; height: 250px; resize: none"></textarea>
-          <input type='submit' name='submit_notes' value='Submit' style='font-size: 1.0em' />
+     <td style="vertical-align: baseline; padding-top: 30px">
+        <form action='/notes.php' method='POST' enctype='multipart/form-data'>
+          <textarea id="notes_textarea" name="notes" placeholder="Enter teacher's comment here ..." style="font-size: 1.5em; width: 450px; height: 250px; resize: none"></textarea>
+          <br/>
+          <br/>
+          <div align="right">
+            <input type='submit' name='submit_notes' value='Submit' style='font-size: 1.5em' />
+          </div>
         </form>
      </td>
-     -->
 
      </tr>
      <table>
