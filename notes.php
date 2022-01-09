2bd6b27b5b7c68b5543e538a22279e9f80c56be3
Mason You <masonyou.su@gmail.com>
2022-01-08 22:34:19 -0500

    added date range to html

--- notes.php
+++ notes.php
@@ -9,6 +9,10 @@
 
    session_start();
 
+   function getStartDateFilterHtmlId() { echo 'date_range_start'; }
+
+   function getStopDateFilterHtmlId()  { echo 'date_range_stop'; }
+
    if ($_SERVER['REQUEST_METHOD'] === 'POST')
    {
       // var_dump($_POST);
@@ -35,9 +39,43 @@
    }
 ?>
 
+     <script type="text/javascript">
+      function getDateString(date_obj)
+      {
+         var yyyy = date_obj.getFullYear().toString();
+         var mm   = (date_obj.getMonth()+1).toString().padStart(2, '0');
+         var dd   = date_obj.getDate().toString().padStart(2, '0');
+
+         var date_str = yyyy + "-" + mm + "-" + dd;
+
+         return date_str;
+      }
+
+      function updateDates()
+      {
+         debugger;
+         var start_date_id = "<?php echo getStartDateFilterHtmlId(); ?>"
+         var start_date_elem = document.getElementById(start_date_id);
+         var stop_date_id = "<?php echo getStopDateFilterHtmlId(); ?>"
+         var stop_date_elem = document.getElementById(stop_date_id);
+
+         var today = new Date();
+         var thirty_dates_ago = new Date();
+         thirty_dates_ago.setDate(thirty_dates_ago.getDate() - 30);
+
+         start_date_elem.value = getDateString(thirty_dates_ago);
+         stop_date_elem.value  = getDateString(today);
+      }
+
+      function on_page_loaded()
+      {
+         updateDates();
+      }
+     </script>
+
   </head>
 
-<body>
+<body onload="on_page_loaded()">
 <?php if ($_SESSION['user_role'] == "student") : ?>
       <h1 align="center">
           You are not allowed to view this page
@@ -57,6 +95,26 @@
             Log Out
            </a>
         </td>
+        </tr>
+
+        <tr>
+           <td>
+              <br/>
+              <label for="start">Date start:</label>
+              <input type="date" value="2022-01-30"
+                 id="<?php echo getStartDateFilterHtmlId(); ?>"
+                 min="2021-12-01"
+                 max="2050-12-31">
+           </td>
+
+           <td>
+              <br/>
+              <label for="stop">Date stop:</label>
+              <input type="date" value="2023-01-30"
+                 id="<?php echo getStopDateFilterHtmlId(); ?>"
+                 min="2021-12-01"
+                 max="2050-12-31">
+           </td>
         </tr>
       </table>
       </div>
