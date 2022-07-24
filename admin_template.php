494f655333107593132fd648bfa432a3ee49604d
Mason You <masonyou.su@gmail.com>
2022-07-24 16:54:54 -0400

    updated admin_template page to callback itself with an action param on the URL

--- admin_template.php
+++ admin_template.php
@@ -1,27 +1,43 @@
 <!--
    this is the common menu/links on the left panel of the page for all admin pages
 -->
+<?php
+   function printHtmlBreaks($num_break)
+   {
+      for ($i=0; $i<$num_break; ++$i)
+      {
+         echo "<br/>";
+      }
+      echo "\n";
+   }
+
+?>
+
 <tr rowspan='99'>
 <td>
+   <?php printHtmlBreaks(10); ?>
+
    <table id='admin_left_panel_table'>
       <tr><td><h1>
-         <a title="Add new student(s)" href='/admin_add_student.php'>Add Student(s)</a>
+         <a title="Add new student(s)" href='/admin.php?action=add'>Add Student(s)</a>
       </h1></td></tr>
 
       <tr><td><h1>
-         <a title="Remove a student" href='/admin_del_student.php'>Remove Student</a>
+         <a title="Remove a student" href='/admin.php?action=del'>Remove Student</a>
       </h1></td></tr>
 
       <tr><td><h1>
-         <a title="change student name" href='/admin_rename_student.php'>Rename Student</a>
+         <a title="change student name" href='/admin.php?action=mod'>Rename Student</a>
       </h1></td></tr>
 
       <tr><td><h1>
-         <a title="Move a student to a different class" href='/admin_mov_student.php'>Move Student</a>
+         <a title="Move a student to a different class" href='/admin.php?action=mov'>Move Student</a>
       </h1></td></tr>
 
       <tr><td><h1>
-         <a title="Change seat assignment for a given class" href='/admin_change_seat.php'>Seat Assignment</a>
+         <a title="Change seat assignment for a given class" href='/admin.php?action=seating'>Seat Assignment</a>
       </h1></td></tr>
    </table>
+
+   <?php printHtmlBreaks(10); ?>
 <td>
