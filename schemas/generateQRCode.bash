8055eb73218ad059efa594160947b3d68ad96a38
Mason You <masonyou.su@gmail.com>
2024-07-20 10:25:01 -0400

    removed class option in db query

--- schemas/generateQRCode.bash
+++ schemas/generateQRCode.bash
@@ -5,16 +5,9 @@ if [ -z "$DATABASE_URL" ]; then
    exit 1
 fi
 
-if [ $# -ne 1 ]; then
-   echo "Usage: $0 [901-904]"
-   exit 2
-fi
+TMP_SCRIPT=qrcode_gen.bash
 
-class=$1
-
-TMP_SCRIPT=qrcode_gen_${class}.bash
-
-db_query="SELECT student_id,fname,lname FROM common.student WHERE class='$class'"
+db_query="SELECT student_id,fname,lname FROM common.student"
 
 ## use "," as separator
 psql --tuples-only -AF$',' $DATABASE_URL -c "$db_query" | \
