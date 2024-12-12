4951778cbdcdff8ebe461140c6754ecfe0396659
Mason You <masonyou.su@gmail.com>
2024-12-11 19:47:40 -0500

    updated rows from 5 to 6, fixed logic on page breaker and added width and height on imgs

--- admin_name_qrcode.php
+++ admin_name_qrcode.php
@@ -7,7 +7,7 @@
    $all_students = getAllStudents();
    $num_students = count($all_students);
 
-   $NUM_ROWS            = 5;
+   $NUM_ROWS            = 6;
    $NUM_COLUMNS_PER_ROW = 2;
 ?>
 
@@ -27,13 +27,17 @@
    break-before: page;
 }
 
+.stud_name {
+   font-weight: bold;
+};
+
 </style>
 
 <table id='main_table' border=1>
    <?php for ($arr_index=0; $arr_index<$num_students; ++$arr_index): ?>
 
        <?php if ($arr_index % $NUM_COLUMNS_PER_ROW == 0) : ?>
-          <?php if ($arr_index % $NUM_ROWS == 0) : ?>
+          <?php if ($arr_index % ($NUM_ROWS * $NUM_COLUMNS_PER_ROW) == 0) : ?>
              <tr class='page_breaker'>
           <?php else : ?>
              <tr>
@@ -43,8 +47,8 @@
           <td class='stud_name'>
              <?php echo $all_students[$arr_index]->fname . " " . substr($all_students[$arr_index]->lname, 0, 1) . ".";  ?>
           </td>
-          <td class='qrcode'>
-             <img src='/imgs/<?php echo $all_students[$arr_index]->student_id . ".png'"; ?>
+          <td class='qrcode_img'>
+             <img src='/imgs/<?php echo $all_students[$arr_index]->student_id . ".png'"; ?> width='145' height='145' />
           </td>
 
        <?php if (($arr_index+1) % $NUM_COLUMNS_PER_ROW == 0) : ?>
